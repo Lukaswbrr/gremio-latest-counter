4ca9f0ba81e176edace2a11440ea6737a2e16be0
Lukaswbrr <49600807+Lukaswbrr@users.noreply.github.com>
2026-02-04 15:28:05 -0300

    testing python api

--- api/scrape_test.py
+++ api/scrape_test.py
@@ -1,18 +1,45 @@
 # api/scrape.py
 from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import requests
 import json
 
 class handler(BaseHTTPRequestHandler):
+    def get_gremio_data(self, headers=None):
+        html = ""
+
+        try:
+            html = requests.get("https://www.fotmob.com/en/teams/9769/overview/gremio", headers=headers)
+            html.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP Error Occured: {err}")
+            return
+
+        soup = BeautifulSoup(html.text, "lxml")
+
+        json_data_element = soup.find("script", id="__NEXT_DATA__")
+        json_data = json.loads(json_data_element.text)
+
+        return json_data
+    
+    def get_latest_matches(self):
+        # gets recent matches
+        recent_matches = self.get_gremio_data()["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["teamForm"]
+        
+        return recent_matches
+
     def do_GET(self):
         # 1. Logic: Fetch the target URL
         url = "https://example.com"
-        # Add headers to mimic a browser (helps avoid some basic blocks)
-        headers = {'User-Agent': 'Mozilla/5.0'}
+
+        ua = UserAgent()
+
+        # # Add headers to mimic a browser (helps avoid some basic blocks)
+        headers = {'User-Agent': ua.random}
         response = requests.get(url, headers=headers)
 
-        # 2. Logic: Parse with BeautifulSoup
+        # # 2. Logic: Parse with BeautifulSoup
         soup = BeautifulSoup(response.text, "html.parser")
         page_title = soup.title.string if soup.title else "No title found"
 
@@ -23,3 +50,12 @@ class handler(BaseHTTPRequestHandler):
         
         data = {"title": page_title}
         self.wfile.write(json.dumps(data).encode())
+
+# def format_match_results(recent_matches):
+#     for k in recent_matches:
+#         match k["resultString"]:
+#             case "W":
+#                 print(f"venceu! ({k['tooltipText']['homeTeam']} {k['tooltipText']['homeScore']} x {k['tooltipText']['awayScore']} {k['tooltipText']['awayTeam']})")
+#             case "L":
+#                 print(f"perdeu! ({k['tooltipText']['homeTeam']} {k['tooltipText']['homeScore']} x {k['tooltipText']['awayScore']} {k['tooltipText']['awayTeam']})")
+        
